3614018fd92d015f7c2a204a99aa7b91289c59dc
Christian Oliveira <christianoliveirati@gmail.com>
2021-01-14 18:14:00 -0300

    :construction: finalizando rota de atualização de vendas

--- src/modules/sales/services/UpdateSaleService.ts
+++ src/modules/sales/services/UpdateSaleService.ts
@@ -5,6 +5,7 @@ import IUpdateSaleDTO from "@modules/sales/dtos/IUpdateSaleDTO";
 import AppError from "@shared/errors/AppError";
 import IRealtyRepository from "@modules/sales/repositories/IRealtyRepository";
 import IClientRepository from "@modules/sales/repositories/IClientRepository";
+import { add } from "date-fns";
 
 interface IRequestDTO {
   id: string;
@@ -37,15 +38,56 @@ class UpdateSaleService {
         realty.id, 
         body.realty
       );
+      delete body.realty;
     }
 
     // Atualizar o cliente comprador
     if (body.client_buyer) {
+      if (body.client_buyer.date_birth) {
+        const ajusted_date = add(
+          body.client_buyer.date_birth, 
+          {hours: 3}
+        )
+        body.client_buyer.date_birth = ajusted_date;
+      }
+
       const client_buyer = saleExists.client_buyer;
       await this.clientsRepository.update(
         client_buyer.id, 
         body.client_buyer
       );
+      delete body.client_buyer;
+    }
+
+    // Atualizar o cliente vendedor
+    if (body.client_seller) {
+      if (body.client_seller.date_birth) {
+        const ajusted_date = add(
+          body.client_seller.date_birth, 
+          {hours: 3}
+        )
+        body.client_seller.date_birth = ajusted_date;
+      }
+
+      const client_seller = saleExists.client_seller;
+      await this.clientsRepository.update(
+        client_seller.id, 
+        body.client_seller
+      );
+      delete body.client_seller;
+    }
+
+    // Atualizar a venda
+    if (Object.keys(body).length !== 0) {
+      if (body.sale_date) {
+        const ajusted_date = add(
+          body.sale_date, 
+          {hours: 3}
+        )
+        body.sale_date = ajusted_date;
+      }
+      
+      await this.salesRepository.update(id, body);
     }
   }
 }
