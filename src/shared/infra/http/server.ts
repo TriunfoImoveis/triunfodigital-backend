df9b3ad761d2dbd55e7a11d958f5c2140accdbe8
Christian Oliveira <christianoliveirati@gmail.com>
2021-01-29 22:36:39 -0300

    :heavy_plus_sign: add lib socket.io e ws(WebSocket) para teste de real-time

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,10 @@
 import 'reflect-metadata';
 import * as dotenv from 'dotenv';
+import * as http from "http";
+import * as io from 'socket.io';
+import path from 'path';
 
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction, Express } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 import { errors } from 'celebrate';
@@ -16,35 +19,68 @@ import '@shared/container/providers';
 
 dotenv.config();
 
-const app = express();
-app.use(cors({ origin: '*' }));
-app.use(express.json());
-app.use('/files', express.static(uploadConfig.tmpFolder));
-app.use(routes);
+class App {
+  public app: Express;
 
-app.use(errors());
+  constructor() {
+    this.app = express();
 
-app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
+    this.routes();
+    this.middlewares();
+    this.execute();
+    this.sockets();
+  }
+
+  routes() {
+    this.app.use(cors({ origin: '*' }));
+    this.app.use(express.json());
+    this.app.use('/files', express.static(uploadConfig.tmpFolder));
+    this.app.use(routes);
+
+    this.app.use(errors());
+  }
+
+  middlewares() {
+    this.app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+      if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+          status: 'error',
+          message: err.message,
+        });
+      }
+
+      console.error(err);
+      return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error.',
+      });
     });
   }
 
-  console.error(err);
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error.',
-  });
-});
+  private async sockets() {
+    const server: http.Server = http.createServer(this.app);
+    const sio: io.Server = new io.Server(server, {transports: ["websocket", "polling"]});
+    
+    sio.on('connection', (socket: io.Socket) => {
+      console.log('connection');
 
-if (process.env.NODE_ENV === 'develop') {
-  app.listen(3335, () => {
-    console.log('Server started on ::PORT 3335!');
-  });
-} else {
-  app.listen(3333, () => {
-    console.log('Server started on ::PORT 3333!');
-  });
+      socket.on('disconnect', () => {
+          console.log('client disconnected');
+      })
+    });
+  }
+
+  execute() {
+    if (process.env.NODE_ENV === 'develop') {
+      this.app.listen(3335, () => {
+        console.log('Server started on ::PORT 3335!');
+      });
+    } else {
+      this.app.listen(3333, () => {
+        console.log('Server started on ::PORT 3333!');
+      });
+    }
+  }
 }
+
+export default new App;
