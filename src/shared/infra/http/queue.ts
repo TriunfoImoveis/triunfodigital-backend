945035b2c908ca61201abc06836d99949d97ab41
Christian Oliveira <christianoliveirati@gmail.com>
2021-03-03 11:39:50 -0300

    :construction: add variaveis de ambientes para ser lidas na queue server

--- src/shared/infra/http/queue.ts
+++ src/shared/infra/http/queue.ts
@@ -1,4 +1,7 @@
 import 'reflect-metadata';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 import '@shared/container/providers';
 import Queue from '@shared/container/providers/JobProvider/implementations/Queue';
